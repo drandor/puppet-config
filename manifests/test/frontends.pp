3e05440aefb5758a8c465f1115f5e137d29e0570
Drandor <drandor@outlook.com>
2012-08-21 23:49:40 +0200

    first client node

--- manifests/test/frontends.pp
+++ manifests/test/frontends.pp
@@ -1,14 +1,8 @@
 # -*- tab-width:3;indent-tabs-mode:nil;  -*-
-#
 # vim: set ts=3 sw=3 tw=0:
 # vim: set expandtab:
 #
 
-import live/*.pp
-
-##
-# A default node
-##
-node default_node inherits default {
+node /puppetc01/ inherits default_node {
 
 }
