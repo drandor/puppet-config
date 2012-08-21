e48c3f58ed7640a35533d2f9132bad68f772de6b
Drandor <drandor@outlook.com>
2012-08-22 00:00:30 +0200

    added runstages

--- manifests/test.pp
+++ manifests/test.pp
@@ -12,3 +12,79 @@ import live/*.pp
 node default_node inherits default {
 
 }
+
+
+##
+## Run Stages
+##
+#
+##
+# There are 10 Stages
+#
+## PreFiles / Bootstrap (name: bootstrap)
+# The bootstrapping stage
+#  - creating users
+#  - creating groups
+#  - ...
+# 
+## Files (name: file)
+# In this stage the configuration is done for things that needs to be configured before the packages are being installed.
+#  - source.list or yum.repos.de
+#  - /etc/default files
+#  - Filesystem Permissions
+#  - ...
+#
+## PostFiles / PrePackages / PreMain (name: prepackage)
+# Things that must be done before the main stage.
+#  - run apt-get update or yum makecache
+#  - there are not much things in this stage (i think)
+#  - ...
+#
+## Main
+# The main stage
+#  - do package installations
+#  - ...
+#
+## PostPackages / PreServices / Configuration (name: configuration)
+# Manage configuration files of daemons, ...
+#  - configure services/daemons
+#  - ...
+#
+## Services (name: service)
+# Start / Stop services
+#  - Add services to runlevels
+#  - Start/Stop services
+#
+## PostServices (name: postservice)
+# This that must be done after service restarts, ...
+#  - Normaly not much in here
+#
+## PreApplication (name: preapp)
+# Do things that have to be done before you deploy your software.
+#  - Creating configuration files
+#  - Remove Systems from Loadbalancer
+#  - ...
+#
+## Application (name: app)
+# Deploy your application
+#  - ...
+#
+## PostApplication (name: postapp)
+# Do things that have to be done after your deployment
+#  - Cache purges
+#  - Add Systems to Loadbalancers
+#  - ...
+
+stage { "bootstrap": before => Stage['file']}
+stage { "file": require => Stage['bootstrap']}
+stage { "prepackage": require => Stage['file']}
+### default: stage { "main": require => Stage['prepackage'] }
+stage { "configuration": require => Stage['main']}
+stage { "service": require => Stage['configuration']}
+stage { "postservice": require => Stage['service']}
+stage { "preapp": require => Stage['postservice']}
+stage { "app": require => Stage['preapp']}
+stage { "postapp": require => Stage['app']}
+
+
+
